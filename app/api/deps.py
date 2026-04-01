c4bb1496539c76a4a57f674d20c8ae49854e73b3
Elizaveta Konovalova <Eliza050203@gmail.com>
2026-04-01 15:05:18 +0300

    realised configs-getting

--- app/api/deps.py
+++ app/api/deps.py
@@ -1,1 +1,9 @@
-# Dependency Injection
+# Dependency Injection (фабрика зависимостей)
+
+from fastapi import Depends
+from typing import Annotated
+
+from app.core.config import get_settings, Settings
+
+
+SettingsDep = Annotated[Settings, Depends(get_settings)]
--- app/core/config.py
+++ app/core/config.py
@@ -1,1 +1,39 @@
 # Конфигурация приложения (env -> Settings)
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from functools import lru_cache
+
+class Settings(BaseSettings):
+    # Настройка загрузки и обработки настроек из .env
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_prefix="",
+        case_sensitive=False
+    )
+
+    # Настройки приложения 
+    app_name: str
+    env: str = 'dev'
+    app_debug : bool = True
+
+    # JWT
+    jwt_secret: str
+    jwt_alg: str
+    access_token_expire_minutes: int = 60
+
+    # DB
+    db_path: str 
+
+    # OpenRouter
+    openrouter_api_key: str
+    openrouter_base_url: str
+    openrouter_model: str
+    openrouter_site_url: str
+    openrouter_app_name: str
+
+@lru_cache
+def get_settings():
+    return Settings()
+
+def get_db_url(settings: Settings) -> str:
+    return f"sqlite+aiosqlite:///{settings.db_path}"
