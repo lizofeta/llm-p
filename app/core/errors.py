6e936765d4cc2665f3d874655f191389b676a61f
Elizaveta Konovalova <Eliza050203@gmail.com>
2026-04-02 10:28:48 +0300

    add domain exceptions

--- app/core/errors.py
+++ app/core/errors.py
@@ -1,1 +1,50 @@
 # Доменные исключения
+
+class LLMPError(Exception):
+    """ Базовая ошибка приложения """
+    def __init__(self, message: str, code: str | None = None):
+        self.message = message
+        self.code = code or self.__class__.__name__
+        super().__init__(message)
+
+# 4xx-like ошибки
+
+class ConflictError(LLMPError):
+    """ 
+    Исключение, возникающее при конфликтах, например:
+    - Пользователь с таким email уже зарегистрирован 
+    - Имя пользователя уже занято 
+    """
+    pass
+
+class UnauthorizedError(LLMPError):
+    """ 
+    Ошибка аутентификации (неверный пароль / токен)
+    """
+    pass
+
+class ForbiddenError(LLMPError):
+    """ Отсутствие прав доступа """
+    pass
+
+class NotFoundError(LLMPError):
+    """ Объект не найден """
+    pass
+
+# Внешние сервисы
+
+class ExternalServiceError(LLMPError):
+    """ Ошибка при обращении к внешнему сервису """
+    def __init__(self, service: str, reason: str):
+        message = f"Ошибка при обращении к сервису '{service}': {reason}"
+        super().__init__(message)
+
+# Более узкие ошибки конкретных кейсов
+
+class EmailAlreadyExistsError(ConflictError):
+    def __init__(self):
+        super().__init__("Пользователь с таким email уже зарегистрирован")
+
+class WrongPasswordError(UnauthorizedError):
+    def __init__(self):
+        super().__init__("Введён неверный пароль")
