3925266ea74929d026633110cb0c168044c71b3a
Elizaveta Konovalova <Eliza050203@gmail.com>
2026-04-04 09:29:36 +0300

    add user and chat_messages repositories

--- app/repositories/chat_messages.py
+++ app/repositories/chat_messages.py
@@ -1,1 +1,50 @@
 # Доступ к таблице чатов
+
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select, delete
+
+from app.schemas.chat import ChatResponse
+from app.db.models import ChatMessage, User
+
+class ChatMessagesStorage:
+    def __init__(self, db: AsyncSession):
+        self._db = db 
+    
+    def _to_response(self, chat_message: ChatMessage) -> ChatResponse:
+        return ChatResponse(
+            answer=chat_message.content
+        )
+    
+    async def add_message(
+            self, 
+            user: User, 
+            message_data: dict[str, str]
+            ) -> ChatResponse:
+        message = ChatMessage(
+            user_id=user.id,
+            role=message_data["role"],
+            content=message_data["content"]
+        )
+        self._db.add(message)
+        await self._db.commit()
+        await self._db.refresh(message)
+        return self._to_response(message)
+    
+    async def show_last_n_messages(
+            self, 
+            user: User, 
+            limit: int
+            ) -> list[ChatResponse]:
+        result = await self._db.scalars(select(ChatMessage)
+                                .where(ChatMessage.user_id == user.id)
+                                .order_by(ChatMessage.id.desc())
+                                .limit(limit))
+        messages = result.all()
+        messages.reverse()
+        return [self._to_response(message) for message in messages]
+    
+    async def delete_chat_history(self, user: User) -> None:
+        await self._db.execute(
+            delete(ChatMessage).where(ChatMessage.user_id == user.id)
+        )
+        await self._db.commit()
--- app/repositories/users.py
+++ app/repositories/users.py
@@ -1,1 +1,45 @@
 # Доступ к таблице users
+
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.db.models import User
+from app.schemas.user import UserPublic
+from app.core.enums import Role
+
+class UserStorage:
+    def __init__(self, db: AsyncSession):
+        self._db = db 
+    
+    def _to_response(self, user: User) -> UserPublic:
+        return UserPublic.model_validate()
+    
+    async def get_user_by_id(self, user_id: int) -> UserPublic | None:
+        user = await self._db.get(User, user_id)
+        if user is None:
+            return None 
+        return self._to_response(user)
+    
+    async def get_user_by_email(self, user_email: str) -> UserPublic | None:
+        query = select(User).where(User.email == user_email)
+        result = await self._db.execute(query)
+        user = result.scalar_one_or_none()
+        if user is None:
+            return None 
+        return self._to_response(user)
+    
+    async def create_user(
+            self,
+            email: str,
+            password_hash: str,
+            role: Role = Role.USER
+            ) -> UserPublic:
+        user = User(
+            email=email,
+            password_hash=password_hash,
+            role=role
+        )
+        self._db.add(user)
+        await self._db.commit()
+        await self._db.refresh(user)
+        return self._to_response(user)
