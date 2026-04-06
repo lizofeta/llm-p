622d6ffa38e9385c78ab123a0512efc059bbf9df
Elizaveta Konovalova <Eliza050203@gmail.com>
2026-04-06 19:49:33 +0300

    add auth and chat usecases

--- app/usecases/auth.py
+++ app/usecases/auth.py
@@ -1,1 +1,82 @@
 # Бизнес логика регистрации и авторизации
+
+from app.repositories.users import UserStorage
+from app.db.models import User
+from app.core.config import Settings
+from app.core.errors import EmailAlreadyExistsError, NotFoundError, UnauthorizedError
+from app.core.security import hash_password, verify_password, create_access_token, decode_access_token
+
+
+class AuthUseCase:
+    def __init__(
+            self, 
+            users_repository: UserStorage,
+            settings: Settings) -> None:
+        self._users_repository = users_repository
+        self._settings = settings
+    
+    async def register(self, email: str, password: str) -> User:
+        # Проверка, что email не занят 
+        existing_user = await self._users_repository.get_user_by_email(email)
+        if existing_user is not None:
+            raise EmailAlreadyExistsError()
+
+        hashed_pwd = hash_password(password)
+        user = (await self._users_repository
+                        .create_user(
+                            email=email, 
+                            password_hash=hashed_pwd
+                            ))
+        return user
+    
+    async def login(self, email: str, password: str) -> str:
+        user = await self._users_repository.get_user_by_email(email)
+        if user is None:
+            raise UnauthorizedError(
+                f"Неверный email или пароль"
+                )
+        
+        if not verify_password(
+            plain_password=password, 
+            hashed_password=user.password_hash):
+            raise UnauthorizedError(
+                "Неверный email или пароль"
+                )
+        
+        access_token = create_access_token(
+            data={"sub": str(user.id),
+                  "role": user.role},
+            expire_minutes=self._settings.access_token_expire_minutes,
+            secret=self._settings.jwt_secret,
+            algorithm=self._settings.jwt_alg
+        )
+
+        return access_token
+    
+    async def get_user_by_id(self, user_id: int) -> User:
+        user = await self._users_repository.get_user_by_id(user_id)
+        if user is None:
+            raise NotFoundError("Пользователь не найден")
+        return user
+    
+    async def get_current_user(self, token: str) -> User:
+        try:
+            payload = decode_access_token(
+                token=token,
+                secret=self._settings.jwt_secret,
+                algorithm=self._settings.jwt_alg
+                )
+        except ValueError as e:
+            raise UnauthorizedError() from e 
+        
+        user_id = payload.get("sub")
+        token_role = payload.get("role")
+        if not user_id or not token_role:
+            raise UnauthorizedError("Некорректный payload токена")
+        
+        db_user = await self.get_user_by_id(int(user_id))
+        
+        if db_user.role != token_role:
+            raise UnauthorizedError("Роль в токене не совпадает с ролью в БД")
+        
+        return db_user
--- app/usecases/chat.py
+++ app/usecases/chat.py
@@ -1,1 +1,79 @@
 # Логика общения с LLM
+
+from app.repositories.chat_messages import ChatMessagesStorage
+from app.services.openrouter_client import OpenRouterClient
+from app.core.enums import MessageRole
+from app.core.config import Settings
+
+class ChatUseCase:
+    def __init__(
+            self, 
+            chat_repository: ChatMessagesStorage,
+            or_client: OpenRouterClient,
+            settings: Settings
+            ) -> None :
+        self._chat_repository = chat_repository
+        self._or_client = or_client
+        self._settings = settings
+
+    async def ask(
+            self,
+            user_id: int,
+            prompt: str,
+            system_prompt: str | None = None 
+            ) -> str:
+        messages: list[dict[str, str]] = []
+
+        # Если есть system-сообщение
+        if system_prompt:
+            messages.append(
+                {
+                    "role": MessageRole.SYSTEM,
+                    "content": system_prompt
+                }
+            )
+        
+        # История сообщений 
+        history = await self._chat_repository.show_last_n_messages(
+            user_id = user_id,
+            limit=self._settings.chat_history_limit
+        )
+
+        for message in history:
+            messages.append(
+                {
+                    "role": message.role,
+                    "content": message.content
+                }
+            )
+        
+        # Текущее сообщение 
+        messages.append(
+            {
+                "role": MessageRole.USER,
+                "content": prompt
+            }
+        )
+
+        # Сохранение промпта в БД
+        await self._chat_repository.add_message(
+            user_id=user_id,
+            role=MessageRole.USER,
+            content=prompt
+        )
+
+        # Запрос к LLM
+        response = await self._or_client.chat_completion(
+            messages=messages
+        )
+
+        content_llm_response = response["choices"][0]["message"]["content"]
+
+        # Сохранение сообщения LLM
+        await self._chat_repository.add_message(
+            user_id=user_id,
+            role=MessageRole.ASSISTANT,
+            content=content_llm_response
+        )
+
+        return content_llm_response
