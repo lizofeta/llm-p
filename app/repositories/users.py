6d0e71620bf43cce742a45705a0f5943bf2d4b19
Elizaveta Konovalova <Eliza050203@gmail.com>
2026-04-06 15:45:32 +0300

    refactor(repo): remove ORM to Pydantic conversion

--- app/repositories/users.py
+++ app/repositories/users.py
@@ -4,36 +4,32 @@ from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.db.models import User
-from app.schemas.user import UserPublic
 from app.core.enums import Role
 
 class UserStorage:
     def __init__(self, db: AsyncSession):
         self._db = db 
     
-    def _to_response(self, user: User) -> UserPublic:
-        return UserPublic.model_validate()
-    
-    async def get_user_by_id(self, user_id: int) -> UserPublic | None:
+    async def get_user_by_id(self, user_id: int) -> User | None:
         user = await self._db.get(User, user_id)
         if user is None:
             return None 
-        return self._to_response(user)
+        return user
     
-    async def get_user_by_email(self, user_email: str) -> UserPublic | None:
+    async def get_user_by_email(self, user_email: str) -> User | None:
         query = select(User).where(User.email == user_email)
         result = await self._db.execute(query)
         user = result.scalar_one_or_none()
         if user is None:
             return None 
-        return self._to_response(user)
+        return user
     
     async def create_user(
             self,
             email: str,
             password_hash: str,
             role: Role = Role.USER
-            ) -> UserPublic:
+            ) -> User:
         user = User(
             email=email,
             password_hash=password_hash,
@@ -42,4 +38,4 @@ class UserStorage:
         self._db.add(user)
         await self._db.commit()
         await self._db.refresh(user)
-        return self._to_response(user)
+        return user
